ccb3b8b1ff8a434e65c61ab939e7f2baf0b04376
Dennis <80940599+wony22@users.noreply.github.com>
2023-06-27 14:28:31 +0700

    Update Config.java

--- src/main/java/org/t246osslab/easybuggy4sb/Config.java
+++ src/main/java/org/t246osslab/easybuggy4sb/Config.java
@@ -4,5 +4,5 @@ public class Config {
     public static final String APP_ROOT = "/eb/v1";
     public static final String TEMPLATE_PREFIX = "eb/v1/";
     public static final String AUTH_USER = "user1";
-    public static final String AUTH_PASS = "pass2";
+    public static final String AUTH_PASS = "pass3";
 }
